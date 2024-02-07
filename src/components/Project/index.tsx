7ff2d0cdce2646c9e2ca7121faa323edf768554a
Flavia Lopes <lalmeida.flavia@gmail.com>
2024-02-06 20:07:35 -0500

    creating projects section and project's component

---
+++ src/components/Project/index.tsx
@@ -0,0 +1,14 @@
+import Paragraph from '../Paragraph'
+import Title from '../Title'
+
+import { Card, ButtonLink } from './styles'
+
+const Project = () => (
+  <Card>
+    <Title>Project Lista de tarefas</Title>
+    <Paragraph type="secondary">Tasks list using VueJS</Paragraph>
+    <ButtonLink>View</ButtonLink>
+  </Card>
+)
+
+export default Project
